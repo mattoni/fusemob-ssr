844624018adadbf55156c6bdc8b8fd84cb6f3e3b
amattoni <amattoni@petrichor.io>
2017-03-25 21:36:45 -0700

    fix bundle

--- fuse-spark.ts
+++ fuse-spark.ts
@@ -24,7 +24,7 @@ lazyLoad.forEach((bundle) => {
     fuse.bundle(`${directory.js}/${name}`)
         .watch()
         .hmr()
-        .instructions(`! [${bundle}]`);
+        .instructions(`[${bundle}]`);
 });
 
 
