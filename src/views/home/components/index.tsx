a3f7f790d7accf5b4e1fc0c722e3434d286fea04
amattoni <amattoni@petrichor.io>
2017-05-02 12:28:59 -0700

    update home style

--- src/views/home/components/index.tsx
+++ src/views/home/components/index.tsx
@@ -1,12 +1,18 @@
+import { px } from 'csx';
 import { observer } from 'mobx-react';
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
+import { style } from 'typestyle';
 
 @observer
 export default class Home extends React.Component<{}, {}> {
     public render() {
+        const homeClass = style({
+            padding: px(10),
+        });
+
         return (
-            <div>
+            <div className={homeClass}>
                 <Helmet>
                     <title>Home</title>
                 </Helmet>
